beca057911a24a61046f37004078774e358bab17
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-03 02:09:14 +0300

    Refactor UserRepository

--- src/main/java/com/epam/jwd/repository/api/UserRepository.java
+++ src/main/java/com/epam/jwd/repository/api/UserRepository.java
@@ -2,21 +2,15 @@ package com.epam.jwd.repository.api;
 
 
 import com.epam.jwd.repository.model.Entity;
-import com.epam.jwd.repository.model.User;
 
 import java.util.List;
 
 public interface UserRepository<V, T extends Entity<V>> {
 
     void save(T user);
-
     T findById(V id);
-
     T findUser(T user);
-
     List<T> findAll();
-
     T findByUserName(String userName);
-
     boolean delete(T user);
 }
