d91bd7b0d88dd532d1932961706e241df9f98eb3
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-03 00:36:22 +0300

    Add static message

--- src/main/java/com/epam/jwd/service/validation/TicketValidation.java
+++ src/main/java/com/epam/jwd/service/validation/TicketValidation.java
@@ -5,7 +5,12 @@ import com.epam.jwd.service.exeption.UnavailableTicketException;
 
 public class TicketValidation {
 
+    private static final String TICKET_UNAVAILABLE_EXCEPTION_MESSAGE = "These ticket isn't available, try any other";
+
     public static boolean isAvailable(Ticket ticket) throws UnavailableTicketException {
-        return ticket.isAvailable();
+        if(ticket.isAvailable()) {
+            return true;
+        }
+        throw new UnavailableTicketException(TICKET_UNAVAILABLE_EXCEPTION_MESSAGE);
     }
 }
