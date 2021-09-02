7f17d42beb23cf80e8a283e0e1acdf875a24de92
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-03 00:44:14 +0300

    Add NoCashException

---
+++ src/main/java/com/epam/jwd/service/exeption/NoCashException.java
@@ -0,0 +1,23 @@
+package com.epam.jwd.service.exeption;
+
+public class NoCashException extends Throwable {
+
+    public NoCashException() {
+    }
+
+    public NoCashException(String message) {
+        super(message);
+    }
+
+    public NoCashException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoCashException(Throwable cause) {
+        super(cause);
+    }
+
+    public NoCashException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
