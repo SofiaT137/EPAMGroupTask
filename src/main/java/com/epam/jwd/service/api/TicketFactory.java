d777a9381ca245c7aa97adcafa5d24895ef870e6
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-03 21:46:15 +0300

    Create ticket factory interface

---
+++ src/main/java/com/epam/jwd/service/api/TicketFactory.java
@@ -0,0 +1,12 @@
+package com.epam.jwd.service.api;
+
+import com.epam.jwd.repository.model.Ticket;
+
+public interface TicketFactory {
+
+    Ticket createMovie1Ticket();
+    Ticket createMovie2Ticket();
+    Ticket createMovie3Ticket();
+    Ticket createMovie4Ticket();
+    Ticket createMovie5Ticket();
+}
