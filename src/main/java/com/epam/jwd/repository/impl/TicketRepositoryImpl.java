936b1efd6d1f1415520cf2616b5e0f399697dada
Alex_Boh <aleksandrbohan2@gmail.com>
2021-09-04 13:14:14 +0300

    I registered the logs in TicketRepositoryImpl class.

--- src/main/java/com/epam/jwd/repository/impl/TicketRepositoryImpl.java
+++ src/main/java/com/epam/jwd/repository/impl/TicketRepositoryImpl.java
@@ -1,14 +1,34 @@
 package com.epam.jwd.repository.impl;
 
+import com.epam.jwd.controller.Controller;
 import com.epam.jwd.repository.api.TicketRepository;
 import com.epam.jwd.repository.model.Ticket;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class TicketRepositoryImpl implements TicketRepository<Long, Ticket> {
+
+    private static final Logger logger = LogManager.getLogger(TicketRepositoryImpl.class);
+
+    private static final String CHECK_FOR_NULL = "Instance is null";
+
+    private static final String ID_SORTING = "ID is searched";
+
+    private static final String REMOVED_TICKET = "Ticket was remove!";
+
+    private static final String ALL_AVAILABLE_TICKETS = "All available tickets are searched!";
+
+    private static final String ALL_AVAILABLE_TICKETS_FOR_KIDS = "All available tickets for kids are searched!";
+
+    private static final String MOVIE_NAME = "Movie was found by movie name";
+
+    private static final String ALL_TICKETS = "There is search all tickets here";
+
 
 
     private static TicketRepositoryImpl instance;
@@ -17,6 +37,7 @@ public class TicketRepositoryImpl implements TicketRepository<Long, Ticket> {
 
     public static TicketRepositoryImpl getInstance() {
         if (instance == null) {
+            logger.log(Level.INFO, CHECK_FOR_NULL);
             instance = new TicketRepositoryImpl();
         }
 
@@ -30,6 +51,7 @@ public class TicketRepositoryImpl implements TicketRepository<Long, Ticket> {
 
     @Override
     public Ticket findById(Long id) {
+        logger.log(Level.DEBUG, ID_SORTING);
         return ticketStorage.stream()
                 .filter(ticket -> id.equals(ticket.getId()))
                 .findFirst()
@@ -38,11 +60,14 @@ public class TicketRepositoryImpl implements TicketRepository<Long, Ticket> {
 
     @Override
     public boolean delete(Ticket ticket) {
+        logger.log(Level.DEBUG, REMOVED_TICKET);
+
         return ticketStorage.remove(ticket);
     }
 
     @Override
     public List<Ticket> findAllAvailable() {
+        logger.log(Level.DEBUG, ALL_AVAILABLE_TICKETS);
         return ticketStorage.stream()
                 .filter(Ticket::isAvailable)
                 .collect(Collectors.toList());
@@ -50,11 +75,13 @@ public class TicketRepositoryImpl implements TicketRepository<Long, Ticket> {
 
     @Override
     public List<Ticket> findAll() {
+        logger.log(Level.DEBUG, ALL_TICKETS);
         return ticketStorage;
     }
 
     @Override
     public List<Ticket> findAllAvailableTicketsForKids() {
+        logger.log(Level.DEBUG, ALL_AVAILABLE_TICKETS_FOR_KIDS);
         return ticketStorage.stream()
                 .filter(Ticket::isAvailableForKids)
                 .collect(Collectors.toList());
@@ -62,6 +89,7 @@ public class TicketRepositoryImpl implements TicketRepository<Long, Ticket> {
 
     @Override
     public Ticket findByMovieName(String movieName) {
+        logger.log(Level.DEBUG, MOVIE_NAME);
         return ticketStorage.stream()
                 .filter(ticket -> movieName.equals(ticket.getMovieName())
                         && ticket.isAvailable())
