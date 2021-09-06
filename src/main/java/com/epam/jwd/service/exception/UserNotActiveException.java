0e3e5caf05c7e59452c7a34269041a7896345f29
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-06 13:30:38 +0300

    Add some default exception constructors

--- src/main/java/com/epam/jwd/service/exception/UserNotActiveException.java
+++ src/main/java/com/epam/jwd/service/exception/UserNotActiveException.java
@@ -1,7 +1,23 @@
 package com.epam.jwd.service.exception;
 
 public class UserNotActiveException extends Exception {
+
     public UserNotActiveException(String message) {
         super(message);
+    }
+
+    public UserNotActiveException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotActiveException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotActiveException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public UserNotActiveException() {
     }
 }
--- src/main/java/com/epam/jwd/service/exception/UserNotFoundException.java
+++ src/main/java/com/epam/jwd/service/exception/UserNotFoundException.java
@@ -1,7 +1,23 @@
 package com.epam.jwd.service.exception;
 
 public class UserNotFoundException extends Exception {
+
     public UserNotFoundException(String message) {
         super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public UserNotFoundException() {
     }
 }
